20798957aade3a41df5f2a3611968aa6b3527385
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-05-10 00:27:03 +0800

    admin

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../app/core/Database.php';
+require_once '../app/core/Controller.php';
+require_once '../app/core/Model.php';
+require_once '../app/core/App.php';
+
+$app = new App();
